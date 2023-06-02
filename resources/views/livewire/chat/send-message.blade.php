f1a09899ea04250fbfa7ded213f601191fe1bb14
ammaar <muhammadfarroos12@gmail.com>
2023-06-02 10:38:02 +0700

    send message design

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,6 +1,11 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
     <div class="chatbox_footer">
-        footer
+
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="write message">
+            <button class="submit" type="submit">Send</button>
+        </div>
+
     </div>
 </div>
